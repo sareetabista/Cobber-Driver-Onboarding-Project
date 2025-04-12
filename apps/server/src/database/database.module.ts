e857ee127808269172f5b8455f87b91bcb71b978
dhiran sapkota <dhiransapkota55@gmail.com>
2025-04-12 15:18:57 +0545

    feat: project setup

---
+++ apps/server/src/database/database.module.ts
@@ -0,0 +1,19 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+
+@Module({
+    imports : [
+        ConfigModule.forRoot({
+            isGlobal : true
+        }),
+        MongooseModule.forRootAsync({
+            imports : [ConfigModule],
+            inject : [ConfigService],
+            useFactory : (configService : ConfigService) => ({
+                uri: configService.get<string>("DB_URI"),
+            })
+        })
+    ]
+})
+export class DatabaseModule {}
---
+++ apps/web/src/App.tsx
@@ -0,0 +1,8 @@
+
+const App = () => {
+  return (
+    <div className="border border-red-600">App</div>
+  )
+}
+
+export default App
